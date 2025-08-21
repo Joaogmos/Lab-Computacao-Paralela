89446e57acfce2407168d237783618705f9d07ae
Joaogmos <160232571+Joaogmos@users.noreply.github.com>
2025-08-21 23:51:04 +0000

    Adicionando as atividades 1 e 2 do laboratorio 1

--- fork_example.c
+++ fork_example.c
@@ -1,19 +1,30 @@
  # include <stdio.h>
  # include <unistd.h>
  # include <sys/wait.h>
+ #include <stdlib.h>
+ #define N 4
 
  int main () {
  pid_t pid = fork ();
+ printf("Processo pai principal com PID %d.\n", getpid());
+ printf("------------------------------------\n");
 
- if ( pid < 0) { // Erro
-    fprintf (stderr , " Fork falhou !\n");
-    return 1;
- } else if (pid == 0) { // Processo Filho
-    printf ("Eu sou o filho ! Meu PID e %d, meu pai e %d.\n", getpid () , getppid ());
- } else { // Processo Pai
-    printf ("Eu sou o pai! Meu PID e %d, criei o filho %d.\n", getpid () , pid);
+ for(int i =0; i < N; i++){
+    pid = fork();
+ 
+    if ( pid < 0) { // Erro
+        fprintf (stderr , " Fork falhou !\n");
+        return 1;
+    } else if (pid == 0) { // Processo Filho
+        printf ("Eu sou o filho ! Meu PID eh %d, meu pai eh %d. e alem disso eu sou o FILHO\n", getpid () , getppid ());
+        exit(0);
+    }
+} 
+for(int i = 0; i < N; i++){ // Processo Pai
+    printf ("Eu sou o pai! Meu PID eh %d, criei o filho %d. Repetindo eu sou o PAI\n", getpid () , pid);
     wait ( NULL ); // Pai espera o filho terminar
-    printf ("Meu filho terminou , agora eu posso terminar .\n");
- }
+    printf ("Meu filho terminou , agora eu posso terminar.\n");
+}
+
  return 0;
 }
